0fe6521ede67c4b057f5f535f7e21e92fddb2769
mgracesb <mercysbarcel@gmail.com>
2020-06-25 17:12:49 +0200

    add styles

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+const display = document.getElementById("display");
+
+function toCamelCase() {
+  const value = input.value;
+  const val = value.replace(/\s/g, "");
+  const x = val.match(/[-_]/) ? val.split(/[-_]/g) : val;
+  const newEl = document.createElement("div");
+  let result;
+  if (x === " " || x === "" || x === null) {
+    result = "";
+  } else if (Array.isArray(x)) {
+    let y = x.slice(0, 1);
+    let z = x.slice(1, x.length);
+    result =
+      x.length >= 2
+        ? y + z.map((i) => i.charAt(0).toUpperCase() + i.slice(1)).join("")
+        : y + z.charAt(0).toUpperCase() + z.slice(1);
+  } else if (x[0].charAt(0) == x[0].charAt(0).toUpperCase()) {
+    result = x.map((i) => i.charAt(0).toUpperCase() + i.slice(1)).join("");
+  } else {
+    result = val;
+  }
+  display.appendChild(newEl);
+  newEl.appendChild(document.createTextNode(`${result}`));
+}
+
+button.addEventListener("click", toCamelCase);
